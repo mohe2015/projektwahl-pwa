53af57018736d578f98729969b15dbf6f0fd1d36
mohe2015 <Moritz.Hedtke@t-online.de>
2020-10-17 22:39:07 +0200

    Add initial flake.nix

---
+++ flake.nix
@@ -0,0 +1,43 @@
+# projektwahl - Diese Software kann eine Projektwahl verwalten, wie sie beispielsweise für eine Projektwoche benötigt wird.
+#
+# Copyright (C) 2020 Moritz Hedtke <Moritz.Hedtke@t-online.de>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+#
+# SPDX-FileCopyrightText: 2020 Moritz Hedtke <Moritz.Hedtke@t-online.de>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }@inputs:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = [
+              pkgs.reuse
+              pkgs.nodejs-14_x
+            ];
+
+            shellHook = ''
+              reuse lint
+            '';
+          };
+        }
+      );
+}
