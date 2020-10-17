99cd015ee373bda224199d9c4c3483310a00286d
mohe2015 <Moritz.Hedtke@t-online.de>
2020-10-17 23:17:28 +0200

    Use bootstrap javascript

--- client/src/index.ts
+++ client/src/index.ts
@@ -26,10 +26,7 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
+import { Toast } from 'bootstrap'
 
-import confetti from 'canvas-confetti';
-
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+Array.from(document.querySelectorAll('.toast'))
+.forEach(toastNode => new Toast(toastNode))
